0785b23b60e668b6a0e7af2b8bd93739abaf3c60
Student <s24545@pjwstk.edu.pl>
2021-10-15 12:48:24 +0200

    zad3

---
+++ 1003.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+auto main () -> int
+
+{
+
+int a,b,c;
+
+cout <<"podaj a: ";
+cin >>a;
+
+cout <<"podaj b: ";
+cin >>b;
+
+c=a*b;
+
+cout << c << "\n";
+}
