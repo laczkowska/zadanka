79f6c2ac7924c2d199b6d4a5a6de3c6c8b735800
Student <s24545@pjwstk.edu.pl>
2021-10-15 13:23:01 +0200

    zad5

---
+++ 1005.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+auto main () -> int
+
+{
+
+int a;
+
+cout <<"podaj a: ";
+cin >>a;
+
+if(a>0){
+  cout << a << "\n";
+}
+auto zmienna = -1;
+if(0>a){
+  cout << a*zmienna << "\n";
+}
+if(a==0){
+  cout << 0 << "\n";
+}
+}
