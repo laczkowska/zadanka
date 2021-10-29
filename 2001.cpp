f0f7ef42b73aa0d2c9578ef70ec9c0afe6c0772a
Student <s24545@pjwstk.edu.pl>
2021-10-29 11:26:42 +0200

    2001

---
+++ 2001.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+auto main () -> int{
+int a,b;
+
+cout <<"podaj a: ";
+cin >>a;
+
+cout <<"podaj b: ";
+cin >>b;
+
+
+	do{
+		cout<<a<<'\n';
+		a++;
+		
+		
+	}while(a<b);
+
+
+
+}
