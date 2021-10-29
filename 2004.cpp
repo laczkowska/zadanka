ee1db2bed141249388304b1f4681058ed93798b3
Student <s24545@pjwstk.edu.pl>
2021-10-29 12:01:57 +0200

    2004

---
+++ 2004.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+bool czy_pierwsza(int a)
+{
+	if(a<2)
+		return false;
+
+	for(int i=2; i*i<=a; i++)
+		if(a%i==0)
+			return false;
+
+	return true; 
+}
+
+int main ()
+{
+	int a;
+
+	cout<<"podaj a: ";
+	cin>>a;
+
+	if (czy_pierwsza(a))
+		cout<<"liczba "<<a<<" jest pierwsza"<<endl;
+	else
+		cout<<"liczba "<<a<<" nie jest pierwsza"<<endl;
+
+return 0;
+}
