800e8055b25f70f53ccfe88fac98b2121c8d0bcb
Student <s24545@pjwstk.edu.pl>
2021-10-29 13:23:52 +0200

    2007

---
+++ 2007.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+auto main () -> int
+
+{
+
+int dzielnik, limit;
+int a;
+int b;
+int test=0;
+int suma=0;
+int i;
+
+dzielnik=a;
+limit=b;
+
+cout <<"podaj dzielnik: ";
+cin >>a;
+
+cout <<"podaj limit: ";
+cin >>b;
+
+             for(int i=0;i<=b;i++){
+                if(i%a==0){	       
+                test=test+1;
+                }
+			 }
+             if(test==0){
+                suma=suma+i;
+                
+             }
+             test=0;
+            
+       
+	   cout<<suma<<"\n";
+
+
+
+
+    }
+
