155e6998439c920956b565c6fec929c0ec7bc739
Student <s24545@pjwstk.edu.pl>
2021-10-29 12:42:40 +0200

    2005

---
+++ 2005.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+bool czy_pierwsza(int a)
+{
+	if(a<2)
+		return false;
+
+	for(int i=2; i*i<=a; i++)
+		if(a%i==0)
+			return false;
+
+	return true; 
+}
+
+int main ()
+{
+	int a;
+    int test=0;
+    int suma=0;
+
+	cout<<"podaj a: ";
+	cin>>a;
+
+	if (czy_pierwsza(a)){
+		cout<<"liczba "<<a<<" jest pierwsza"<<endl;
+
+
+        for(int j=2;j<=a;j++){
+	        for(int i=2; i*i<=j; i++){
+		        if(j%i==0){
+                    test=test+1;
+                }
+			 }
+             if(test==0){
+                suma=suma+j;
+                
+             }
+             test=0;
+            
+        }
+	   cout<<suma;
+
+
+
+
+    }
+	else
+		cout<<"liczba "<<a<<" nie jest pierwsza"<<endl;
+
+
+return 0;
+}
