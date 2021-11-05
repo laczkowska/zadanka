13a34383513647160f4ad80d9e242ba819ee6a70
Student <s24545@pjwstk.edu.pl>
2021-11-05 13:26:19 +0100

    2009

--- 2009.cpp
+++ 2009.cpp
@@ -11,11 +11,13 @@ int a, silnia, b;
 cout <<"podaj liczbe do silni: ";
 cin >>b ;
 
-while ( a <= b) {
-    silnia=silnia*a;
-a-- ;
 a = 1;
 silnia = 1;
+
+while ( a <= b) {
+    silnia=silnia*a;
+a++ ;
+
 }
 cout << silnia << "\n";
 
