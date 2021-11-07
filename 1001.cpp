3e5e8448c4230f397116619a54d92fa06a1169e7
laczkowska <91725304+laczkowska@users.noreply.github.com>
2021-11-07 19:10:16 +0100

    Add files via upload

--- 1001.cpp
+++ 1001.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
+
 auto main () -> int
+
 {
-std::cout << "Hello,  World !" << "\n " ;
-
-auto value = std :: string {};
-std :: getline ( std :: cin , value );
-return std :: stoi ( value );
+       std::cout << "czesc lubie kebab" << "\n";
 }
-
