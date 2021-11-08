5658dfeb57b58d0b7cccc16a8a175b7ad39ce87b
laczkowska <91725304+laczkowska@users.noreply.github.com>
2021-11-08 11:30:22 +0100

    Add files via upload

--- 2004.cpp
+++ 2004.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
+
 using namespace std;
 
 bool czy_pierwsza(int a)
 {
-	if(a<2)
-		return false;
+        if (a < 2)
+            return false;
 
-	for(int i=2; i*i<=a; i++)
-		if(a%i==0)
-			return false;
+        for (int i=2; i*i<=a; i++)
+            if (a%i==0)
+                return false;
 
-	return true; 
+        return true;
 }
 
 int main ()
 {
-	int a;
 
-	cout<<"podaj a: ";
-	cin>>a;
+    int a;
 
-	if (czy_pierwsza(a))
-		cout<<"liczba "<<a<<" jest pierwsza"<<endl;
-	else
-		cout<<"liczba "<<a<<" nie jest pierwsza"<<endl;
+    cout << "podaj a: ";
+    cin >>a;
+
+    if (czy_pierwsza(a))
+        cout << "liczba " << a << " jest pierwsza" << '\n';
+    else
+        cout << "liczba " << a << " nie jest pierwsza" << '\n';
+
 
 return 0;
+
 }
+
+
+
+
