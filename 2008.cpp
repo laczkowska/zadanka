6e32e3e2b24b75f4cc60ae0d87c8c6e404c41579
laczkowska <91725304+laczkowska@users.noreply.github.com>
2021-11-08 13:14:55 +0100

    Add files via upload

--- 2008.cpp
+++ 2008.cpp
@@ -2,21 +2,22 @@
 
 using namespace std;
 
-int main () 
+auto main () -> int
 
 {
 
-int a, silnia, b;
+int a, b, silnia;
 
-cout <<"podaj liczbe do silni: ";
-cin >>b ;
+cout << "podaj liczbe do silni: ";
+cin >>b;
 
 silnia = 1;
 
-for (a=1; a<=b; a++){
+for (a=1; a <= b; a++){
     silnia=silnia*a;
 }
-cout << silnia << "\n";
+
+cout << silnia << '\n';
 
 return 0;
 
