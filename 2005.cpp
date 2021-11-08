dd540f8168de5a17688dda1b49122dcfcbe3a8ba
laczkowska <91725304+laczkowska@users.noreply.github.com>
2021-11-08 12:14:37 +0100

    Add files via upload

--- 2005.cpp
+++ 2005.cpp
@@ -1,53 +1,58 @@
 #include <iostream>
+
 using namespace std;
 
 bool czy_pierwsza(int a)
 {
-	if(a<2)
-		return false;
+{
+        if (a < 2)
+            return false;
 
-	for(int i=2; i*i<=a; i++)
-		if(a%i==0)
-			return false;
+        for (int i=2; i*i<=a; i++)
+            if (a%i==0)
+                return false;
 
-	return true; 
+        return true;
+}
 }
 
 int main ()
+
 {
-	int a;
+        int a;
     int test=0;
     int suma=0;
 
-	cout<<"podaj a: ";
-	cin>>a;
+        cout << "podaj a: ";
+        cin >>a;
+{
+        if (czy_pierwsza(a))
+            cout << "liczba " << a << " jest pierwsza" << '\n';
 
-	if (czy_pierwsza(a)){
-		cout<<"liczba "<<a<<" jest pierwsza"<<endl;
 
-
-        for(int j=2;j<=a;j++){
-	        for(int i=2; i*i<=j; i++){
-		        if(j%i==0){
-                    test=test+1;
-                }
-			 }
-             if(test==0){
-                suma=suma+j;
-                
+        for(int j=2; j<=a; j++){
+             for(int i=2; i*i<=j; i++){
+                    if(j%i==0){
+                test=test+1;
              }
-             test=0;
-            
-        }
-	   cout<<suma;
+                    }
+            if(test == 0){
+               suma+suma+j;
+
+            }
+            test=0;
+
+        
+            cout << suma;
 
 
+    
+    
+            else
+            {
+                cout << "liczba " << a << " nie jest pierwsza" << '\n';
 
-
-    }
-	else
-		cout<<"liczba "<<a<<" nie jest pierwsza"<<endl;
-
-
-return 0;
+            }
+}
+}
 }
