3faf7454b6c2a5980e964199c29ba078c48e9162
laczkowska <91725304+laczkowska@users.noreply.github.com>
2021-11-08 11:04:57 +0100

    Add files via upload

--- 2002.cpp
+++ 2002.cpp
@@ -2,33 +2,36 @@
 
 using namespace std;
 
-auto main () -> int{
-int a,b,c;
+auto main () -> int
 
-cout <<"podaj a: ";
+{
+
+int a, b, c;
+
+cout << "podaj a: ";
 cin >>a;
 
-cout <<"podaj b: ";
+cout << "podaj b: ";
 cin >>b;
 
-cout <<"podaj c: ";
+cout << "podaj c: ";
 cin >>c;
 
 if (c==0){
-	cout<< "zle c rowne 0" << "\n";
+    std::cout <<  "zle c rowne 0"  << "\n";
 }
 else{
-	do{
-		if((a%c)==0){
-			cout<<a<<'\n';
-			
-		}
-		a++;
-	}while(a<b);
-}
+        do{
+                if((a%c)==0){
+                    cout << a << '\n';
 
+
+                }
+                a++;
+       }while(a<b);
+}
 
 return 0;
-}
 
+}
 
