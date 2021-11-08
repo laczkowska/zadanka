e9222d32fcdc5b7027474fd0829354c4b6b6a721
laczkowska <91725304+laczkowska@users.noreply.github.com>
2021-11-08 12:28:25 +0100

    Add files via upload

--- 2007.cpp
+++ 2007.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-auto main() -> int
+auto main () -> int
 
 {
 
@@ -10,23 +10,21 @@ int a, b;
 int test=0;
 int suma=0;
 
-
-
-cout <<"podaj dzielnik: ";
+cout << "podaj dzielnik: ";
 cin >>a;
 
-cout <<"podaj limit: ";
+cout << "podaj limit: ";
 cin >>b;
-    
-    for(int i=1; i<=b; i++)
+
+    for ( int i=1; i<=b; i++)
     {
-  
-        if(i%a==0){
+
+        if ( i%a==0){
             suma=suma+i;
-            
-            
+
+
         }
     }
-       cout << suma << "\n";
+       cout << suma << '\n';
     return 0;
 }
