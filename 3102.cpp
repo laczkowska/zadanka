9dca61d4d93682fe42bc7d850c9e8c6f540dedd0
laczkowska <91725304+laczkowska@users.noreply.github.com>
2021-11-12 13:16:28 +0100

    Add files via upload

---
+++ 3102.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{ 
+    const int max=100;
+    float tab[max];
+    int n, a;
+
+        cout << "podaj jak dluga ma byc tablica: ";
+        cin >> n;
+    do
+    {
+        
+        if (n<=0 || n>max)
+            cout << "zle liczba mniejsza od 0 lub wieksza od 100, po za zakresem" << '\n';;
+    }
+    while (n==0 || n>max);
+
+    for (int i=0; i<n; i++)
+    {
+        cout << "podaj liczbę start: " << '\n';
+        cin >> a;
+        cout << '\n';
+
+        for (int a=0; a<=n; a++)
+        {
+            cout << a << '\n';
+            
+        }
+    }
+
+return 0;
+
+}
+
