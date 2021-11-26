80f44a5dfdb1429d436af092f6de7bba7d89505a
laczkowska <91725304+laczkowska@users.noreply.github.com>
2021-11-26 10:52:31 +0100

    Add files via upload

--- 3201.cpp
+++ 3201.cpp
@@ -9,11 +9,14 @@ int main ()
 ;
 
         array <char, 94> tab;
-    for (int i=33; i<=126; i++)
+
+    for (int i>=33; i<=126; i++)
     {
 
        tab.at ((i-33)) = char(i);
+
             cout << tab.at(i-33) << '\n';
+
     }
 
 
