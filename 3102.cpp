149ebdbcaf816ef6d5c36416909719767bdcc83f
laczkowska <91725304+laczkowska@users.noreply.github.com>
2022-01-13 11:32:37 +0100

    Add files via upload

--- 3102.cpp
+++ 3102.cpp
@@ -2,42 +2,28 @@
 
 using namespace std;
 
-int main ()
-
+auto main() -> int
 {
-
-    const int max=100;
-    float tab[max];
-    int n;
-
-        cout << "podaj wielkosc tablicy: ";
-        cin >> n;
-
-    do
-    {
+	int A[100];
+    int a, s, i;
     
-        if (n<=0 || n>max)
-            cout << "zle liczba mniejsza od 0 lub wieksza od 100, po za zakresem " <<'\n';
-           
-        else 
-            cout << "tablica zatwierdzona" << '\n';
-            
-    }
-    while (n==0 || n>max);
+    cout << "podaj wielkosc tablicy: " << '\n';
+    cin >> a;
 
-    for (int i=1; i++;)
-    {
-        cout << "podaj " << i << " element" << '\n';
-        cin >> tab[i];
-        cout << '\n';
-
-        for (int a=i; a<=n; a++)
+        if(a > 100)
         {
-        cout << a << '\n';
+            cout << "zle, liczba nie moze byc wieksza od 100 " << '\n'; 
+            cin >> a;
         }
-    }
 
-return 0;
+        else
+        {
+            cout << "wprowadz jedna wartosc tablicy: " << '\n';
+            cin >> s;
 
+
+            A[i]=s;         
+            s++;
+        }
+    return 0;
 }
-
