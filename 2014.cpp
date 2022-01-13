9ba43f1362052ef9ea10e2f5e499ee0485d6199d
laczkowska <91725304+laczkowska@users.noreply.github.com>
2022-01-13 11:25:12 +0100

    Add files via upload

--- 2014.cpp
+++ 2014.cpp
@@ -6,46 +6,28 @@ auto main () -> int
 
 {
 
-int a;
+    int a;
+    cout << "podaj długość boku kwadratu: " << '\n';
+    cin >> a;
 
-cout << "podaj dlugosc boku a: ";
-cin >>a;
-
-if (a<3)
-    cout << "zle a mniejsze od 3 " << '\n';
-
-else
-{
-    for (int n=1; n<a; n++)
+    for(int i = 0; i < a; i++)        
     {
         cout << "*";
     }
-    for (int j=0; j<a-1; j++)
-    {
-        cout << "*";
-        for (int i=2; i<=a-1; i++)
-        {
-            cout << " ";
-        }
-        cout << "*" << '\n';
-    }
-
-    for (int n=1; n<=a; n++)
-    {
-        cout << "*";
-        }
         cout << '\n';
+
+    for(int j = 0; j < a - 2; j++) 
+    {
+        cout<<"*";
+            for(int j = 0; j < a - 2; j++)  
+            {
+                cout << " ";
+            }
+        cout << "*\n";
     }
+    for(int k = 0; k < a; k++) 
+        cout << "*";
+    
 
 return 0;
-
 }
-
-
-
-
-
-
-
-
-
