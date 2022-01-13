5d46a84a0584a298f7a1b55b67ecebfa3ea69081
laczkowska <91725304+laczkowska@users.noreply.github.com>
2022-01-13 11:19:38 +0100

    Add files via upload

--- 2005.cpp
+++ 2005.cpp
@@ -2,57 +2,46 @@
 
 using namespace std;
 
-bool czy_pierwsza(int a)
+auto main() -> int
 {
-{
-        if (a < 2)
-            return false;
+    int a, suma=0;
 
-        for (int i=2; i*i<=a; i++)
-            if (a%i==0)
-                return false;
+    cout << "podaj liczbę: " << '\n';
+    cin >> a;
 
-        return true;
+	if(a%2==0 && a != 2 && a != 3 && a != 5 && a != 7 && a != 11 || a%3==0 && a != 2 && a != 3 && a != 5 && a != 7 && a != 11 || a%5==0 && a != 2 && a != 3 && a != 5 && a != 7 && a != 11 || a%7==0 && a != 2 && a != 3 && a != 5 && a != 7 && a != 11 ||  a%11==0 && a != 2 && a != 3 && a !=5 && a != 7 && a != 11 )
+	{
+	    cout << "nie jest to liczba pierwsza :(" << '\n';
+		return 0;
+	}
+
+	else
+	{
+		cout << "jest to liczba pierwsza :)" << '\n';
+	}
+	
+    for(int i=2;i<=a;i++)
+    {
+	    if(i%2==0 || i%3==0 || i%5==0 || i%7==0 || i%11==0)
+	    {
+		    if(i==2 || i==3 || i==5 || i==7 || i==11)
+		    {
+			    suma = suma + i;
+			
+			    }
+	cout << " " << '\n';
+	}
+
+	else
+	{
+		suma = suma + i;
+		
+	}
+} 
+
+    cout << suma << '\n';
+
+return 0;
 }
-}
-
-int main ()
-
-{
-        int a;
-    int test=0;
-    int suma=0;
-
-        cout << "podaj a: ";
-        cin >>a;
-{
-        if (czy_pierwsza(a))
-            cout << "liczba " << a << " jest pierwsza" << '\n';
 
 
-        for(int j=2; j<=a; j++){
-             for(int i=2; i*i<=j; i++){
-                    if(j%i==0){
-                test=test+1;
-             }
-                    }
-            if(test == 0){
-               suma+suma+j;
-
-            }
-            test=0;
-
-        
-            cout << suma;
-
-
-    
-    
-            else
-            {
-                cout << "liczba " << a << " nie jest pierwsza" << '\n';
-
-            }
-}
-}
-}
